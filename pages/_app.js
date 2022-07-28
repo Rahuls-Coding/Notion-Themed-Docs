b19aeebaed34deaad7bc76063dd54f92fa659fed
Rahul <rahulscodingworld@gmail.com>
2022-07-28 16:05:22 +0530

    added explaination of EOAs & wallets

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import '../styles/globals.css'
+import { MantineProvider } from '@mantine/core';
+import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <MantineProvider withGlobalStyles withNormalizeCSS theme={{fontFamily: 'PT Serif, sans-serif',
+}}> <ThemeProvider> <Component {...pageProps} /> </ThemeProvider> </MantineProvider>
 }
 
 export default MyApp
